52a8a3bad8af2d96bbf10d3a4df7c9c8699fd06c
Anupam Kumar Lal <anupam.kumarlal@mastercard.com>
2024-09-06 18:50:00 +0530

    Package.swift file added

---
+++ ios-transfer-sdk/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ConnectTransfer",
+    platforms: [.iOS(.v14)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ConnectTransfer",
+            targets: ["ConnectTransfer"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/atomicfi/atomic-transact-ios.git", from: "3.3.4")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "ConnectTransfer",
+            path: "ConnectTransfer.xcframework"),
+    ]
+)
